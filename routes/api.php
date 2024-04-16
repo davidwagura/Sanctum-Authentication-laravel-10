3e379bcd663b0b6ba8d881b01c5715000da36584
David wanjohi <davidwagura83@gmail.com>
2024-04-16 11:49:31 +0300

    Create Api routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+});
